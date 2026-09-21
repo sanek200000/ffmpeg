cad77f9b2cabde152468277cc8805dd150ae27ab
heist <heist2000@gmail.com>
2026-09-21 15:56:45 +0400

    add counter to converter

--- convert_all_files_here.py
+++ convert_all_files_here.py
@@ -1,29 +1,26 @@
 import subprocess
 from pathlib import Path
 
+TEST = True
 CURRENT_DIR = Path(__file__).resolve().parent
-SUFFIX = "*.mkv"
+SUFFIX = "*.txt"
 PREFIX = "out_"
 OUT_SUFFIX = ".mkv"
 
 
 def get_command(filepath: Path, out_filepath: Path) -> str:
-    return f"""ffmpeg -i \
-            '{filepath}' \
-            -map 0:v:0 \
-            -map 0:a:6 \
-            -map 0:s \
-            -map_metadata -1 -map_chapters -1 \
-            -filter:v "setpts=PTS/1.25" \
-            -filter:a "atempo=1.25" \
-            -c:v libx264 -crf 18 -preset medium -pix_fmt yuv420p \
-            -c:a ac3 -b:a 192k \
+    return f"""ffmpeg -i '{filepath}' \
+            ... \
             '{out_filepath}'
         """
 
 
 if __name__ == "__main__":
-    for filepath in CURRENT_DIR.glob(SUFFIX):
+    files = sorted(CURRENT_DIR.glob(SUFFIX))
+    for i, filepath in enumerate(files, start=1):
+        if TEST and i > 1:
+            break
+
         stem = filepath.stem
         out_filepath = filepath.parent.joinpath(PREFIX + stem + OUT_SUFFIX)
 
